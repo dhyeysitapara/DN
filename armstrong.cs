6d04ae4ae5dac9514f576bc0fe1bd3aa2c1d471f
Dhyey <dsitapara@gmail.com>
2025-07-24 09:06:51 +0530

    armstrong

---
+++ armstrong.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DN
+{
+    internal class armstrong
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int originalNum = num;
+            int sum = 0;
+
+            while (num > 0)
+            {
+                int digit = num % 10;
+                sum += digit * digit * digit; // Cube of digit
+                num /= 10;
+            }
+
+            if (sum == originalNum)
+                Console.WriteLine("Armstrong number");
+            else
+                Console.WriteLine("Not an Armstrong number");
+        }
+    }
+}
---
+++ samenumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DN
+{
+    internal class samenumber
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+
+            Console.Write("OUTPUT: ");
+            foreach (char digit in input)
+            {
+                switch (digit)
+                {
+                    case '0': Console.Write("Zero "); break;
+                    case '1': Console.Write("One "); break;
+                    case '2': Console.Write("Two "); break;
+                    case '3': Console.Write("Three "); break;
+                    case '4': Console.Write("Four "); break;
+                    case '5': Console.Write("Five "); break;
+                    case '6': Console.Write("Six "); break;
+                    case '7': Console.Write("Seven "); break;
+                    case '8': Console.Write("Eight "); break;
+                    case '9': Console.Write("Nine "); break;
+                    default: Console.Write("? "); break; // Handles unexpected characters
+                }
+            }
+        }
+    }
+}
