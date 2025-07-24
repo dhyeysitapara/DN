6082dd5ce451591dfd228c4706b99c05a47b4f19
Dhyey <dsitapara@gmail.com>
2025-07-24 08:55:58 +0530

    fibonacci

---
+++ same.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DN
+{
+    internal class same
+    {
+        static void Main()
+        {
+            // Ask the user for their name
+            Console.Write("Enter your name: ");
+
+            // Read the name from console input
+            string name = Console.ReadLine();
+
+            // Print the name
+            Console.WriteLine("OUTPUT: " + name);
+        }
+
+    }
+}
